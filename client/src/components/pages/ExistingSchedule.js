2e71c11c903f53dda9b66b2273f64d2904fd982e
Aryan <2002aryank@gmail.com>
2022-01-25 12:52:33 -0500

    nothing much

--- client/src/components/pages/ExistingSchedule.js
+++ client/src/components/pages/ExistingSchedule.js
@@ -23,13 +23,13 @@ const ExistingSchedule = (props) => {
   let min_hour = 24;
   let max_hour = 0;
 
-  for(const evt of schedEvents){
-    if (evt.scheduleName === scheduleName){
+  for (const evt of schedEvents) {
+    if (evt.scheduleName === scheduleName) {
       const start_hr = evt.startHour;
       const end_hr = evt.endHour;
 
-      if(start_hr <  min_hour) min_hour = start_hr;
-      if(end_hr > max_hour) max_hour = end_hr;
+      if (start_hr < min_hour) min_hour = start_hr;
+      if (end_hr > max_hour) max_hour = end_hr;
     }
   }
 
@@ -38,10 +38,10 @@ const ExistingSchedule = (props) => {
 
   const time_slots = [];
   var counter = 0;
-  for(let num = min_hour * 60; num <= max_hour * 60; num+=15){
-    const hr = Math.floor(num/60);
+  for (let num = min_hour * 60; num <= max_hour * 60; num += 15) {
+    const hr = Math.floor(num / 60);
     const min = num - 60 * hr;
-    time_slots[counter] = ""+hr+":"+(min === 0 ? "00": ("" + min));
+    time_slots[counter] = "" + hr + ":" + (min === 0 ? "00" : "" + min);
     counter++;
   }
 
@@ -68,59 +68,87 @@ const ExistingSchedule = (props) => {
       return 6;
     }
   };
-  
+
   const calculate_row_span = (evt) => {
-    const num = ((evt.endHour * 60 + evt.endMinute) - (evt.startHour * 60 + evt.startMinute))/15;
+    const num = (evt.endHour * 60 + evt.endMinute - (evt.startHour * 60 + evt.startMinute)) / 15;
     return num;
-  }
+  };
 
   const convert_time_to_num = (hours, min) => {
     return 60 * hours + min;
-  }
+  };
 
   const convert_time_to_ampm = (time) => {
     const hrs = parseInt(time.substring(0, time.indexOf(":")));
-    const min = parseInt(time.substring(time.indexOf(":")+1));
-    return "" + (hrs % 12 === 0 ? 12 : hrs % 12) + ":" + (min === 0 ? "00" : min) + (hrs >= 12 ? "pm" : "am");
-  }
+    const min = parseInt(time.substring(time.indexOf(":") + 1));
+    return (
+      "" +
+      (hrs % 12 === 0 ? 12 : hrs % 12) +
+      ":" +
+      (min === 0 ? "00" : min) +
+      (hrs >= 12 ? "pm" : "am")
+    );
+  };
 
   let i = 0;
   let univ_table = [];
 
   const get_table_contents = (time) => {
-      const elts_of_week = [<td>{0 === parseInt(time.substring(time.indexOf(":")+1)) ? convert_time_to_ampm(time) : ""}</td>, <td className=" no-events" rowSpan={1} />, <td className=" no-events" rowSpan={1} />, <td className=" no-events" rowSpan={1} />, <td className=" no-events" rowSpan={1} />, <td className=" no-events" rowSpan={1} />, <td className=" no-events" rowSpan={1} />, <td className=" no-events" rowSpan={1} />]
-      const evts=[0,0,0,0,0,0,0]
+    const elts_of_week = [
+      <td>
+        {0 === parseInt(time.substring(time.indexOf(":") + 1)) ? convert_time_to_ampm(time) : ""}
+      </td>,
+      <td className=" no-events" rowSpan={1} />,
+      <td className=" no-events" rowSpan={1} />,
+      <td className=" no-events" rowSpan={1} />,
+      <td className=" no-events" rowSpan={1} />,
+      <td className=" no-events" rowSpan={1} />,
+      <td className=" no-events" rowSpan={1} />,
+      <td className=" no-events" rowSpan={1} />,
+    ];
+    const evts = [0, 0, 0, 0, 0, 0, 0];
 
-      for(const evt of schedEvents){
-        if ((evt.scheduleName === scheduleName) && evt.startHour === parseInt(time.substring(0, time.indexOf(":"))) && evt.startMinute === parseInt(time.substring(time.indexOf(":")+1))){
-          var correct_index = dayToNum(evt.day) + 1;
-          for(const week of univ_table){
-            for(const week_evt of week){
-              if(dayToNum(week_evt.day) < dayToNum(evt.day) && convert_time_to_num(week_evt.endHour, week_evt.endMinute) > convert_time_to_num(evt.startHour,evt.startMinute) ){
-                correct_index--;
-                elts_of_week.pop();
-              }
+    for (const evt of schedEvents) {
+      if (
+        evt.scheduleName === scheduleName &&
+        evt.startHour === parseInt(time.substring(0, time.indexOf(":"))) &&
+        evt.startMinute === parseInt(time.substring(time.indexOf(":") + 1))
+      ) {
+        var correct_index = dayToNum(evt.day) + 1;
+        for (const week of univ_table) {
+          for (const week_evt of week) {
+            if (
+              dayToNum(week_evt.day) < dayToNum(evt.day) &&
+              convert_time_to_num(week_evt.endHour, week_evt.endMinute) >
+                convert_time_to_num(evt.startHour, evt.startMinute)
+            ) {
+              correct_index--;
+              elts_of_week.pop();
             }
           }
-          elts_of_week[correct_index] = 
+        }
+        elts_of_week[correct_index] = (
           <td className=" has-events" rowSpan={calculate_row_span(evt)}>
-            <div className="row-fluid lecture" style={{width: '99%', height: '99%'}}>
-              <span className="title">{evt.eventName}</span> <span className="time_design">
-                  {evt.startHour % 12 == 0 ? 12 : evt.startHour % 12}:
-                  {evt.startMinute == 0 ? "00" : evt.startMinute}
-                  {evt.startHour >= 12 ? "pm" : "am"} - {evt.endHour % 12 == 0 ? 12 : evt.endHour % 12}:
-                  {evt.endMinute == 0 ? "00" : evt.endMinute}
-                  {evt.endHour >= 12 ? "pm" : "am"}
+            <div className="row-fluid lecture" style={{ width: "99%", height: "99%" }}>
+              <span className="title">{evt.eventName}</span>{" "}
+              <span className="time_design">
+                {evt.startHour % 12 == 0 ? 12 : evt.startHour % 12}:
+                {evt.startMinute == 0 ? "00" : evt.startMinute}
+                {evt.startHour >= 12 ? "pm" : "am"} -{" "}
+                {evt.endHour % 12 == 0 ? 12 : evt.endHour % 12}:
+                {evt.endMinute == 0 ? "00" : evt.endMinute}
+                {evt.endHour >= 12 ? "pm" : "am"}
               </span>
             </div>
-          </td>;
-          evts[dayToNum(evt.day)] = evt;
-        }
+          </td>
+        );
+        evts[dayToNum(evt.day)] = evt;
       }
-      univ_table[i] = [...evts];
-      i++;
+    }
+    univ_table[i] = [...evts];
+    i++;
 
-      return <tr>{elts_of_week.map(elt => (elt))}</tr>;
+    return <tr>{elts_of_week.map((elt) => elt)}</tr>;
   };
 
   return (
@@ -158,7 +186,7 @@ const ExistingSchedule = (props) => {
           </tbody>
         </table>
       </div>
-  
+
       {/* OLD CODE BELOW */}
 
       <div className="new_schedule_flex">
@@ -210,11 +238,11 @@ const ExistingSchedule = (props) => {
         </section>
       </div>
       <section className="back_button_container">
-          <Link to="/schedules">
-            <button type="button" className="back_button_design">
-              Back to <b>My Schedules</b>
-            </button>
-          </Link>
+        <Link to="/schedules">
+          <button type="button" className="back_button_design">
+            Back to <b>My Schedules</b>
+          </button>
+        </Link>
       </section>
     </div>
   );
